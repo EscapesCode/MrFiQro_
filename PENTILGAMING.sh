ee5026c8c5780a8b7d165827f8acdd33546a4a73
pentilgaming <73970978+pentilgaming@users.noreply.github.com>
2020-11-05 07:39:28 +0800

    Add files via upload

---
+++ PENTILGAMING.sh
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay by FIQRO
+echo $green"BALIK LAGI SAMA GUA DISINI AJG"
+echo 
+sleep 2
+echo $red"PILIH OPSI NYA SAYANG EMHHH YAMETE KUDASAIII :D"
+echo
+echo $cyan"1.)STABILKAN HUBUNGAN .... EH ASU JARINGAN MKSDKU :V"
+echo  
+echo $blue"0.)YAMETTEEE KUDASAIIII (BERENTI AJA MASS MPPSSS)"
+echo
+echo $white
+read -p "PILIH MANA  :" DATTEBAYOOO !
+
+if  [  $DATTEBAYOOO = 1 ] || [ $DATTEBAYOOO = 1]
+then
+clear
+toilet -f big -F gay FIQRO
+echo $blue"STABILKAN HUBUNGAN... WE ASU JARINGAN AJG .. BY PENTIL GAMING"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $DATTEBAYOOO = 0 ] || [ $DATTEBAYO00 = 0 ]
+then
+clear
+echo $yellow
+figlet "BAPAK KAU MAEN BOLA PAKE SARUNG"
+echo $red"XIXI CANDA LURD.... DADAHHH "
+sleep 2
+exit
+fi
+
